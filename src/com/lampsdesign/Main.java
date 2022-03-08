49e11a0aed9f38e5832711204f11258e52f7081d
Adam Lamprell <adam.lamprell@gmail.com>
2022-03-08 13:46:08 +0000

    TEST

---
+++ src/com/lampsdesign/Main.java
@@ -0,0 +1,17 @@
+package com.lampsdesign;
+
+public class Main {
+
+    public static final String DB_NAME = "messaging.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:F:\\Java Projects\\SQLDatabase\\" + DB_NAME;
+
+    public static final String TABLE_CONTACTS = "contacts";
+
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_EMAIL = "email";
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
